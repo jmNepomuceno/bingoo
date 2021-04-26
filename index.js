7a3c3408fe915d78b7ea4cb0f5604772df6737cd
John Marvin Nepomuceno <jmgnngmj@gmail.com>
2021-04-26 21:53:07 +0800

    42621

--- index.js
+++ index.js
@@ -17,29 +17,45 @@ for(let card = 1; card <= 2; card++){
 }
 
 // setting numbers for each box
-let b_numbers_1_cards = []
-let i_numbers_1_cards = []
-let n_numbers_1_cards = []
-let g_numbers_1_cards = []
-let o_numbers_1_cards = []
+const chooseYourCards = document.getElementById('get-card-btn')
+const play_btn = document.getElementById('play-btn')
+const roll_btn = document.getElementById('roll-btn')
 
-let b_numbers_2_cards = []
-let i_numbers_2_cards = []
-let n_numbers_2_cards = []
-let g_numbers_2_cards = []
-let o_numbers_2_cards = []
+chooseYourCards.addEventListener('click', generateCards , false)
 
-generateNumbers(1 ,'B', 1 , 15 , b_numbers_1_cards)
-generateNumbers(1 ,'I', 16 ,30 , i_numbers_1_cards)
-generateNumbers(1 ,'N', 31 , 45 , n_numbers_1_cards)
-generateNumbers(1 ,'G', 46 , 60 , g_numbers_1_cards)
-generateNumbers(1 ,'O', 61 , 75 , o_numbers_1_cards)
+play_btn.addEventListener('click' , function(){
+    chooseYourCards.style.pointerEvents = 'none'
+    play_btn.style.pointerEvents = 'none'
+    roll_btn.style.pointerEvents = 'auto'
+}, false)
 
-generateNumbers(2 ,'B', 1 , 15 , b_numbers_2_cards)
-generateNumbers(2 ,'I', 16 ,30 , i_numbers_2_cards)
-generateNumbers(2 ,'N', 31 , 45 , n_numbers_2_cards)
-generateNumbers(2 ,'G', 46 , 60 , g_numbers_2_cards)
-generateNumbers(2 ,'O', 61 , 75 , o_numbers_2_cards)
+function generateCards(){
+    var b_numbers_1_cards = []
+    var i_numbers_1_cards = []
+    var n_numbers_1_cards = []
+    var g_numbers_1_cards = []
+    var o_numbers_1_cards = []
+
+    var b_numbers_2_cards = []
+    var i_numbers_2_cards = []
+    var n_numbers_2_cards = []
+    var g_numbers_2_cards = []
+    var o_numbers_2_cards = []
+
+    generateNumbers(1 ,'B', 1 , 15 , b_numbers_1_cards)
+    generateNumbers(1 ,'I', 16 ,30 , i_numbers_1_cards)
+    generateNumbers(1 ,'N', 31 , 45 , n_numbers_1_cards)
+    generateNumbers(1 ,'G', 46 , 60 , g_numbers_1_cards)
+    generateNumbers(1 ,'O', 61 , 75 , o_numbers_1_cards)
+
+    generateNumbers(2 ,'B', 1 , 15 , b_numbers_2_cards)
+    generateNumbers(2 ,'I', 16 ,30 , i_numbers_2_cards)
+    generateNumbers(2 ,'N', 31 , 45 , n_numbers_2_cards)
+    generateNumbers(2 ,'G', 46 , 60 , g_numbers_2_cards)
+    generateNumbers(2 ,'O', 61 , 75 , o_numbers_2_cards)
+
+    play_btn.style.pointerEvents = 'auto'
+}
 
 
 function generateNumbers(card_number , letter , start , end, letter_arr){
@@ -86,29 +102,14 @@ for(let i of bingo_arr){
 
 // roll for next ball or number
 // roll for next letter first
-const roll_btn = document.getElementById('roll-btn')
 
 let b_numbers = []
 let i_numbers = []
 let n_numbers = []
 let g_numbers = []
 let o_numbers = []
+let letter_number_rolled
 
-// for(let i = 1; i <= 15; i++){
-//     b_numbers.push(i)
-// }
-// for(let i = 16; i <= 30; i++){
-//     i_numbers.push(i)
-// }
-// for(let i = 31; i <= 45; i++){
-//     n_numbers.push(i)
-// }
-// for(let i = 46; i <= 60; i++){
-//     g_numbers.push(i)
-// }
-// for(let i = 61; i <= 75; i++){
-//     o_numbers.push(i)
-// }
 
 roll_btn.addEventListener('click' , function(){
     const bingo_title = document.querySelectorAll('.bingo-title-letters')
@@ -149,9 +150,14 @@ roll_btn.addEventListener('click' , function(){
 function roll_numbers(letter_rolled, temp_numbers_arr){
     let number_index = 0
     let number_timer = 0
+    const prev_rolled = document.getElementById('previous-rolled')
     let number_interval = setInterval(function(){
         const bingo_numbers = document.querySelectorAll('.bingo-body-letter-' + letter_rolled + '-boxes')
         //const bingo_numbers = document.querySelectorAll('.bingo-body-letter-B-boxes')
+
+        //disable roll button
+
+        roll_btn.style.pointerEvents = "none"
 
         bingo_numbers[number_index].style.borderColor = 'white'
         for(let i of temp_numbers_arr){
@@ -176,65 +182,16 @@ function roll_numbers(letter_rolled, temp_numbers_arr){
                 for(let i of temp_numbers_arr){
                     bingo_numbers[i].style.borderColor = 'red'
                 }
-                console.log(temp_numbers_arr)
-                console.log(number_index)
+                letter_number_rolled = letter_rolled + " " + bingo_numbers[number_index].textContent
+                prev_rolled.textContent = letter_number_rolled
+                roll_btn.style.pointerEvents = "auto"
                 clearInterval(number_interval)
             }
         }
     }, 10)
+
+    //display the previous rolls
+    
+
 }
-
-// roll_btn.addEventListener('click' , function(){
-//     const bingo_title = document.querySelectorAll('.bingo-title-letters')
-//     let letter_index = 0
-//     let timer = 0
-//     let number_timer = 0
-//     let letter_rolled
-
-//     let letter_interval = setInterval(function(){
-
-//         bingo_title[letter_index].style.borderColor = '#3e3f42'
-//         letter_index = Math.floor(Math.random() * 5);
-//         bingo_title[letter_index].style.borderColor = '#ff9900'
-//         letter_rolled = bingo_title[letter_index].textContent
-//         timer += 1
-
-//         if(timer == 40){
-//             timer = 0
-//             clearInterval(letter_interval)
-//             let number_index = 0
-//             let number_interval = setInterval(function(){
-//                 //const bingo_numbers = document.querySelectorAll('.bingo-body-letter-' + letter_rolled + '-boxes')
-//                 const bingo_numbers = document.querySelectorAll('.bingo-body-letter-B-boxes')
-
-//                 bingo_numbers[number_index].style.borderColor = 'white'
-//                 number_index = Math.floor(Math.random() * 15)
-//                 bingo_numbers[number_index].style.borderColor = '#ff9900'
-//                 number_timer += 1
-
-//                 if(number_timer == 40){
-//                     number_timer = 0
-        
-//                     if(number_rolled.includes(number_index)){
-//                         // console.log("as")
-//                         while(number_rolled.includes(number_index)){
-//                             // console.log("here")
-//                             number_index = Math.floor(Math.random() * 15)
-//                         }
-//                     }else{
-//                         number_rolled.push(number_index)
-//                         console.log(number_index)
-//                         clearInterval(number_interval)
-//                     }
-//                 }
-//             },10)
-            
-//         }
-
-//     } , 10)
-      
-// }, false)
-
-//
-
 
